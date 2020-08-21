6e5f7d01b6eef801fb971a5f8a7df8f1be1c234f
Promo <admin@easy0502.ispiria.net>
2020-08-21 13:23:58 +0300

    Initial commit

--- js/jquery/ui/core.js
+++ js/jquery/ui/core.js
@@ -1,1 +1,7 @@
-../../../bottom.js
+(new CookieConsent()).init();
+var orderForm = new OrderForm();
+orderForm.init();
+document.addEventListener('DOMContentLoaded', function () {
+    orderForm.init();
+    orderForm.listenMessages();
+});
